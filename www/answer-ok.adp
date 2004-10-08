b7f608ced13178722045ab301ee00cd35a03f435
Jose Pablo Escobedo Del Cid <jose.escobedo@int-edu.eu>
2004-10-08 23:32:06 +0000

    page to inform the student that the answer was sent successfully

---
+++ www/answer-ok.adp
@@ -0,0 +1,6 @@
+<master>
+<property name="title">#evaluation.Answer_1#</property>
+<property name="context">#evaluation.Answer_1#</property>
+
+
+#evaluation.Your#
---
+++ www/answer-ok.tcl
@@ -0,0 +1,15 @@
+# /packages/evaluation/www/answer-ok.tcl
+
+ad_page_contract {
+	
+    Displays a success message to the student
+
+    @author jopez@galileo.edu
+    @creation-date Oct 2004
+    @cvs-id $Id$
+
+} -query {
+	return_url:notnull
+}
+
+ad_return_template
