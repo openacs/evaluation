cb75ca5267e1a021effbbcd433278fc557d5a15f
Jose Pablo Escobedo Del Cid <jose.escobedo@int-edu.eu>
2004-12-06 16:43:04 +0000

    adding missing set of variable community_id

--- www/admin/evaluations/student-list.tcl
+++ www/admin/evaluations/student-list.tcl
@@ -19,6 +19,7 @@ ad_page_contract {
 
 set user_id [ad_conn user_id]
 db_1row get_task_info { *SQL* }
+set community_id [dotlrn_community::get_community_id]
 
 set page_title "[_ evaluation.lt_Students_List_for_tas]"
 set context [list "[_ evaluation.lt_Students_List_for_tas]"]
@@ -302,7 +303,6 @@ if { $number_of_members > 1 } {
     }
     set sql_query [db_map sql_query_groups]
 } else {
-    set community_id [dotlrn_community::get_community_id]
     if { [llength $done_students] > 0 } {
 	set not_in_clause [db_map not_yet_in_clause_non_empty]
     } else {
